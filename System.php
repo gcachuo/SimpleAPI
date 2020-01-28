8b52c993554c689b67486e739c60902dac025a6f
Memo Cachu <gcachu.o@gmail.com>
2020-01-27 18:42:29 -0600

    defined

--- System.php
+++ System.php
@@ -14,7 +14,7 @@ class System
 
     static function decrypt($value_encrypted)
     {
-        define('SEED', 'crypt0w4113t');
+        if (!defined('SEED')) define('SEED', 'crypt0w4113t');
         $value_encrypted = html_entity_decode($value_encrypted);
         $value = openssl_decrypt($value_encrypted, "AES-256-CBC", SEED, 0, str_pad(SEED, 16, 'X', STR_PAD_LEFT));
         return $value;
