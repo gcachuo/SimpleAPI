c84c2d7c734dc58eafb74cfc1621f89d1317affb
Memo Cachu <gcachu.o@gmail.com>
2020-01-31 16:23:13 -0600

    config

--- scripts/web.sh
+++ scripts/web.sh
@@ -5,3 +5,7 @@ mkdir -p ../../modules;
 mkdir -p ../../themes;
 
 cp ../web/index.php ../../
+cp ../web/config.json ../../
+cp -avr ../web/modules/* ../../modules/
+
+wget -O ../../logo.png https://picsum.photos/300/300
--- web/index.php
+++ web/index.php
@@ -1,4 +1,4 @@
 <?php
-include __DIR__ . "/../api/Lib/System.php";
+include __DIR__ . "/core/System.php";
 $system = new System();
 $system->init_web(['WEBDIR' => __DIR__]);
