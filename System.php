96fe508677e2cd105f8ef331eff5b77a446b9b5a
Memo Cachú <gcachu.o@gmail.com>
2020-05-05 00:41:37 -0500

    get ip

--- System.php
+++ System.php
@@ -19,6 +19,17 @@ class System
      */
     private $dom;
 
+    static function getRealIP()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))
+            return $_SERVER['HTTP_CLIENT_IP'];
+
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+        return $_SERVER['REMOTE_ADDR'];
+    }
+
     static function decrypt(string $value_encrypted)
     {
         $value_encrypted = html_entity_decode($value_encrypted);
