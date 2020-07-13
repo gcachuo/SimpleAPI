2fc4b4b520df73fad4e434be15964f83fdeeaeb2
Memo Cachú <gcachu.o@gmail.com>
2020-07-13 13:00:59 -0500

    service worker

--- web/index.php
+++ web/index.php
@@ -3,3 +3,9 @@ include __DIR__ . "/core/System.php";
 
 define('SESSIONCHECK', true);
 System::init_web(['WEBDIR' => __DIR__]);
+?>
+<script>
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js');
+    }
+</script>
