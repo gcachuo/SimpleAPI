3eee58405857e12f6dedfcd86ce59c8efb46faa4
Memo Cachú <gcachu.o@gmail.com>
2020-08-27 15:43:13 -0500

    abstract

--- classes/Socket.php
+++ classes/Socket.php
@@ -5,8 +5,12 @@ use Ratchet\ConnectionInterface;
 
 abstract class Socket
 {
+    protected static $connections;
+
     public function __construct()
     {
+        self::$connections = new SplObjectStorage();
+
         /*$server = $_SERVER;
         if ($_SERVER['HTTP_UPGRADE'] == 'websocket') {
             try {
@@ -16,6 +20,37 @@ abstract class Socket
             }
         }
         throw new CoreException('This is not a websocket connection', 500, compact('server'));*/
+    }
+
+    function onMessage(ConnectionInterface $from, $msg)
+    {
+        echo $msg . PHP_EOL;
+        /** @var ConnectionInterface $client */
+        foreach (self::$connections as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                [$event_name, $data_object] = json_decode($msg, true);
+                $client->send(json_encode([$event_name, $data_object]));
+            }
+        }
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        self::$connections->attach($conn);
+
+        echo 'new connection' . PHP_EOL;
+        $conn->send(json_encode(['message', ['message' => 'Welcome!']]));
+    }
+
+    function onClose(ConnectionInterface $conn)
+    {
+        self::$connections->detach($conn);
+    }
+
+    function onError(ConnectionInterface $conn, Exception $e)
+    {
+        // TODO: Implement onError() method.
     }
 
     public static function open()
