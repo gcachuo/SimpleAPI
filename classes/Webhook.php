ccf2af6644d4acadc3c02f7a39ba8dab76f3b207
Memo Cachú <gcachu.o@gmail.com>
2020-09-09 13:45:39 -0500

    webhooks platform

--- classes/Webhook.php
+++ classes/Webhook.php
@@ -2,10 +2,18 @@
 
 class Webhook
 {
-    public function __construct()
+    public function __construct(string $platform)
     {
-        ['events' => $events, 'key' => $key] = json_decode(file_get_contents(DIR . '/Config/webhook_events.json'), true);
+        ['events' => $events, 'key' => $key] = json_decode(file_get_contents(DIR . '/Config/webhook_events.json'), true)[$platform];
         ['controller' => $controller, 'action' => $action] = $events[$_POST[$key]];
+
+        if (empty($controller)) {
+            throw new CoreException('Empty Controller', 500, $events[$_POST[$key]]);
+        }
+        if (empty($action)) {
+            throw new CoreException('Empty Action', 500, $events[$_POST[$key]]);
+        }
+
         $controller = 'Controller\\' . $controller;
 
         /** @var Controller $class */
