bc8c0e5f7d5bb64046d62adff2fc42feaa52da8e
Memo Cachú <gcachu.o@gmail.com>
2021-02-02 14:30:33 -0600

    update

--- web/assets/src/typings/globals.d.ts
+++ web/assets/src/typings/globals.d.ts
@@ -5,7 +5,19 @@ interface ApiResponse<T = { [name: string]: object | number | string }> {
 }
 
 interface JQuery {
-    select2(options);
+    select2();
+
+    select2(event: 'data', data: object[]);
+
+    select2(options: {
+        placeholder?: string,
+        tags?: boolean,
+        ajax?: { url: string, dataType: string, data?, processResults },
+        maximumSelectionLength?: number,
+        multiple?: boolean,
+        width?: string,
+        dropdownAutoWidth?: boolean,
+    });
 
     modal();
 
