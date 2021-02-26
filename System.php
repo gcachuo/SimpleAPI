01770f6b439616f888b72f34e96d7fe3f13b13f1
Memo Cachú <gcachu.o@gmail.com>
2021-02-26 17:44:52 -0600

    fix composeradded x-database to access control

--- System.php
+++ System.php
@@ -652,7 +652,7 @@ class System
             header('Content-Type: application/json');
             header('Access-Control-Allow-Origin: *');
             header('Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, PATCH, DELETE');
-            header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, dataType, contenttype, processdata, authorization, x-client');
+            header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, dataType, contenttype, processdata, authorization, x-client, x-database');
         }
         register_shutdown_function(function () {
             if (error_get_last()) {
