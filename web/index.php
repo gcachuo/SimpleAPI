c9f6d905051b46980c590eea5ec29202a93f2473
Memo Cachú <gcachu.o@gmail.com>
2024-01-17 12:49:57 -0600

    update index

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
-include __DIR__ . "/core/System.php";
+include __DIR__ . '/core/System.php';
 
-define('SESSIONCHECK', "user_token");
-System::init_web(['WEBDIR' => __DIR__]);
+['file' => $file, 'module_file' => $module_file, 'theme' => $theme] = System::init_web(['WEBDIR' => __DIR__]);
+
+System::formatDocument($file, $module_file, $theme);
 
