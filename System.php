eedabcad9b1d2cc1177c7dc0c304770fb135a825
Memo Cachú <gcachu.o@gmail.com>
2026-03-13 02:21:38 -0600

    timeout 5

--- System.php
+++ System.php
@@ -240,7 +240,7 @@ class System
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 5,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => $options['method'] ?? 'GET',
