cd254b0517420759960b4e4528a5682af6b07242
Memo Cachú <gcachu.o@gmail.com>
2026-07-14 11:23:45 -0600

    fix: reemplazar utf8_decode() deprecated con mb_convert_encoding() para PHP 8.2+

--- System.php
+++ System.php
@@ -903,9 +903,9 @@ class System
 
     public static function utf8($value)
     {
-        if (mb_detect_encoding(utf8_decode($value)) === 'UTF-8') {
+        if (mb_detect_encoding(mb_convert_encoding($value, 'ISO-8859-1', 'UTF-8')) === 'UTF-8') {
             // Double encoded, or bad encoding
-            $value = utf8_decode($value);
+            $value = mb_convert_encoding($value, 'ISO-8859-1', 'UTF-8');
         }
 
         include_once __DIR__ . '/vendor/neitanod/forceutf8/src/ForceUTF8/Encoding.php';
